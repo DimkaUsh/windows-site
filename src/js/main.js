f01cbbc8743513c05c1efc4c48eadded583e2500
Dmitrii Ushakov <81690819+DimkaUsh@users.noreply.github.com>
2022-06-17 13:03:01 +0800

    feat: add second tabs panel and active class for active tab

--- src/js/main.js
+++ src/js/main.js
@@ -4,5 +4,16 @@ import tabs from './modules/tabs.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   popups();
-  tabs();
+  /* Glazing tabs */
+  const TAB_GLAZING_NAMES = ['tree', 'aluminum', 'plastic', 'french', 'rise'];
+  const glazingContent = document.querySelectorAll('.glazing_content');
+  const tabGlazingBtns = document.querySelectorAll('.slider_btn');
+  const activeGlazingTabClass = 'active';
+  tabs(TAB_GLAZING_NAMES, glazingContent, tabGlazingBtns, activeGlazingTabClass);
+  /* Decoration tabs */
+  const TABS_DECORATION_NAMES = ['internal', 'external', 'rising', 'roof'];
+  const decorationContent = document.querySelectorAll('.decoration_card');
+  const tabDecorationBtns = document.querySelectorAll('.decoration_btn');
+  const activeDecoratioTabClass = 'after_click';
+  tabs(TABS_DECORATION_NAMES, decorationContent, tabDecorationBtns, activeDecoratioTabClass);
 });
--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,26 +1,28 @@
 import comparisonDataName from './service/comparisonDataName';
 import changeDisplay from './service/changeDisplay.js';
 
-const tabs = () => {
-  const TABS_NAMES = ['tree', 'aluminum', 'plastic', 'french', 'rise'];
-  const glazingContent = document.querySelectorAll('.glazing_content');
-  const tabBtns = document.querySelectorAll('.slider_btn');
-
-  const changeGlazingContent = (tabBtn, content) => {
+const tabs = (TAB_NAMES, tabContent, tabBtns, tabBtnActiveClass) => {
+  const changeTabContent = (tabBtn, content) => {
     tabBtn.addEventListener('click', (e) => {
-      glazingContent.forEach((content) => {
+      tabContent.forEach((content) => {
         changeDisplay(content, 'none');
       });
+
+      tabBtns.forEach((tab) => {
+        tab.classList.remove(tabBtnActiveClass);
+      });
+
       if (e.target) {
         e.preventDefault();
         changeDisplay(content, 'block');
       }
+      tabBtn.classList.add(tabBtnActiveClass);
     });
   };
 
   tabBtns.forEach((tabBtn) => {
-    const content = comparisonDataName(TABS_NAMES, tabBtn);
-    changeGlazingContent(tabBtn, content);
+    const content = comparisonDataName(TAB_NAMES, tabBtn);
+    changeTabContent(tabBtn, content);
   });
 };
 
